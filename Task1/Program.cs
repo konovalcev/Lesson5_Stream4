3625c16ff5deb4653277d70697ad87b1c3e0ab81
aleks <akonovalcev@gmail.com>
2018-10-11 00:29:18 +0400

    Started Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Реализовать перевод из 10 в 2 систему счисления с использованием стека.
+
+// Александр Коновальцев
+
+namespace Task1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+
+    }
+}
